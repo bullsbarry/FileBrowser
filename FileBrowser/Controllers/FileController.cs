1fd5bec7217d84bfe794e9f4df8417432010184d
Barry Bulls <bullsbarry@gmail.com>
2017-11-26 23:28:08 -0500

    Add swagger and basic model for file system browsing.

---
+++ FileBrowser/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using FileBrowser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FileBrowser.Controllers
+{
+    [RoutePrefix("api/v1/files")]
+    public class FileController : ApiController
+    {
+        private const string RootPath = "";
+
+        /// <summary>
+        /// Takes a file path and returns a list of files and directories on the server under that path.
+        /// </summary>
+        /// <param name="path">The path to list.</param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{*path}")]
+        public IHttpActionResult List(string path)
+        {
+            try
+            {
+                return Ok(FileManager.GetFileList(path));
+            }
+            catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+    }
+}
---
+++ FileBrowser/Models/FileModels.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FileBrowser.Models
+{
+    public class FSResult
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastAccessTime { get; set; }
+        public DateTime LastWriteTime { get; set; }
+    }
+
+    public class FileResult : FSResult
+    {
+        public long Length { get; set; }
+    }
+
+    public class DirectoryResult : FSResult
+    {
+
+    }
+
+    public class ListResult
+    {
+        public DirectoryResult Current { get; set; }
+        public List<FileResult> Files { get; set; } = new List<FileResult>();
+        public List<DirectoryResult> Directories { get; set; } = new List<DirectoryResult>();
+    }
+
+
+    public class FileManager
+    {
+        private static string RootPath = ConfigurationManager.AppSettings["FileSystemRoot"];
+
+        public static ListResult GetFileList(string Path)
+        {
+            var result = new ListResult();
+
+            var listPath = RootPath + Path;
+            var directoryInfo = new DirectoryInfo(listPath);
+
+            if (directoryInfo.Exists)
+            {
+                result.Current = new DirectoryResult()
+                {
+                    Name = directoryInfo.Name,
+                    Created = directoryInfo.CreationTime,
+                    LastAccessTime = directoryInfo.LastAccessTime,
+                    LastWriteTime = directoryInfo.LastWriteTime
+                };
+
+                foreach (var directory in directoryInfo.GetDirectories())
+                {
+                    result.Directories.Add(new DirectoryResult()
+                    {
+                        Name = directory.Name,
+                        Created = directory.CreationTime,
+                        LastAccessTime = directory.LastAccessTime,
+                        LastWriteTime = directory.LastWriteTime
+                    });
+                }
+
+                foreach (var file in directoryInfo.GetFiles())
+                {
+                    result.Files.Add(new FileResult()
+                    {
+                        Name = file.Name,
+                        Created = file.CreationTime,
+                        LastAccessTime = file.LastAccessTime,
+                        LastWriteTime = file.LastWriteTime,
+                        Length = file.Length
+                    });
+                }
+            }
+
+            return result;
+        }
+    }
+}
